7eb4f0b4cef1479b0e0cdbf7c24c0b7b3acf145e
Lemath <56204810+Lemath@users.noreply.github.com>
2021-10-15 12:21:31 -0400

    friday cleanup

--- residential_controller.py
+++ residential_controller.py
@@ -1,35 +1,38 @@
+#from time import sleep
+
 class Column:
-    def __init__(self, _id, _amountOfFloors, _amountOfElevators, _status='online'):
+
+    def __init__(self, _id, _amountOfFloors, _amountOfElevators, _status='online', _bottomFloor=1):
         self.ID = _id
         self.status = _status
         self.amountOfFloors = _amountOfFloors
         self.amountOfElevators = _amountOfElevators
+        self.bottomFloor = _bottomFloor
+        self.topFloor = self.bottomFloor + self.amountOfFloors - 1
         self.elevatorList = []
         self.callButtonList = []
         self.createElevators()
         self.createCallButtons()
 
     def createCallButtons(self):
-        buttonFloor = 1
+        
         callButtonID = 1
-        for i in range(self.amountOfFloors):
-            if buttonFloor < self.amountOfFloors:
-                callButton = CallButton(callButtonID, buttonFloor, 'up')
-                self.callButtonList.append(callButton)
-            if buttonFloor > 1:
-                callButton = CallButton(callButtonID, buttonFloor, 'down')
-                self.callButtonList.append(callButton)
+        for floor in range(1,self.amountOfFloors):    
+            self.callButtonList.append(CallButton(callButtonID, floor, 'up'))
             callButtonID += 1
-            buttonFloor += 1
+        for floor in range(2, self.amountOfFloors + 1):
+            self.callButtonList.append(CallButton(callButtonID, floor, 'down'))
+            callButtonID += 1
+            
 
     def createElevators(self):
-        elevatorID = 1
-        for i in range(self.amountOfElevators):
-            elevator = Elevator(elevatorID, self.amountOfFloors, 'idle', 1)
-            self.elevatorList.append(elevator)
-            elevatorID += 1
+        
+        for id in range(1, self.amountOfElevators + 1):
+            self.elevatorList.append(Elevator(id, self.amountOfFloors, 'idle', 1))
+            
 
     def requestElevator(self, requestedFloor, direction):
+
         bestElevator = self.findElevator(requestedFloor, direction)
         bestElevator.floorRequestList.append(requestedFloor)
         bestElevator.move()
@@ -37,7 +40,8 @@ class Column:
         return bestElevator
 
     def findElevator(self, requestedFloor, requestedDirection):
-        bestElevator = {
+
+        comparedElevator = {
             'elevator' : None,
             'score' : 5,
             'referenceGap' : 10000000
@@ -52,10 +56,13 @@ class Column:
                 score = 2
             elif elevator.status == 'idle':
                 score = 3
-            bestElevator = elevator.checkIfElevatorIsBetter(score, bestElevator, requestedFloor)
+            bestElevator = elevator.compareElevator(score, comparedElevator, requestedFloor)
+            comparedElevator = bestElevator
+
         return bestElevator['elevator']
 
 class Elevator:
+
     def __init__(self, _id, _amountOfFloors, _status='idle', _currentFloor=1):
         self.ID = _id
         self.status = _status
@@ -63,21 +70,19 @@ class Elevator:
         self.currentFloor = _currentFloor
         self.direction = None
         self.overweightAlarm = False
-        self.overweight = False
+        self.overweightSensor = 'OFF'
         self.door = Door(self.ID)
         self.floorRequestButtonList = []
         self.floorRequestList = []
         self.createFloorRequestButtons()
 
     def createFloorRequestButtons(self):
-        buttonFloor = 1
-        floorRequestButtonID = 1
-        for i in range(self.amountOfFloors):
-            self.floorRequestButtonList.append(FloorRequestButton(floorRequestButtonID, buttonFloor))
-            buttonFloor += 1
-            floorRequestButtonID += 1
 
-    def checkIfElevatorIsBetter(self, scoreToCheck, bestElevator, floor):
+       for idAndFloor in range(1, self.amountOfFloors + 1):
+           self.floorRequestButtonList.append(FloorRequestButton(idAndFloor, idAndFloor))
+
+    def compareElevator(self, scoreToCheck, bestElevator, floor):
+
         if scoreToCheck < bestElevator['score']:
             bestElevator['score'] = scoreToCheck
             bestElevator['elevator'] = self
@@ -90,11 +95,13 @@ class Elevator:
         return bestElevator
 
     def requestFloor(self, requestedFloor):    
+
         self.floorRequestList.append(requestedFloor)
         self.move()
         self.operateDoors()
 
     def move(self):
+
         while len(self.floorRequestList) > 0:
             destination = self.floorRequestList.pop(0)
             self.status = 'moving'
@@ -114,47 +121,60 @@ class Elevator:
         self.status = 'idle'
 
     def sortFloorList(self):
+
         if self.direction == 'up':
             self.floorRequestList.sort()
         else:
             self.floorRequestList.sort(reverse=True)
 
     def operateDoors(self):
+
         self.door.status = 'opened'
-        # Wait 5 Seconds
-        if not self.overweight:
+        #sleep(5)
+        if not self.isOverweight():
             self.door.status = 'closing'
-            if not self.door.obstructed:
+            if not self.door.isObstructed():
                 self.door.status = 'closed'
             else:
                 self.operateDoors()
         else:
-            while self.overweight:
+            while self.isOverweight():
                 self.overweightAlarm = True
             self.overweightAlarm = False
             self.operateDoors()
 
+    def isOverweight(self):
+        
+        return self.overweightSensor == 'ON' 
+
 
 class CallButton:
+
     def __init__(self, _id, _floor, _direction, _status='OFF'):
+
         self.ID = _id
         self.floor = _floor
         self.direction = _direction
         self.status = _status
 
 class FloorRequestButton:
+
     def __init__(self, _id, _floor, _status='OFF'):
+
         self.ID = _id
         self.floor = _floor
         self.status = _status
 
 
 class Door:
+
     def __init__(self, _id, _status='closed'):
+
         self.ID = _id
         self.status = _status
-        self.obstructed = False
+        self.obstructionSensorState = 'OFF'
     
 
-    def isObstructed(): 
-        return False
+    def isObstructed(self): 
+
+        return self.obstructionSensorState == 'ON'
